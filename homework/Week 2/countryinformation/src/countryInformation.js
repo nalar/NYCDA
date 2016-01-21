785b6800d1400db7deb2a86386839df27e3737be
Nalar <thijstel@gmail.com>
2016-01-21 12:06:15 +0100

    Countryinformation working both sync and async

--- homework/Week 2/countryinformation/src/countryInformation.js
+++ homework/Week 2/countryinformation/src/countryInformation.js
@@ -4,7 +4,18 @@ var jsonfilereader = require('../../json-file-reader/src/json-file-reader.js');
 var targetCountry = process.argv[2];
 
 //Sync
+// var countrylist = (jsonfilereader.jsObject("countries.json"));
 
+// function targetTLD(targetCountry){
+// 	for (var i = 0; i < countrylist.length; i++) {
+// 		if(countrylist[i].name === targetCountry){
+// 			return(countrylist[i].topLevelDomain);
+// 		}
+// 	}
+// };
+
+// console.log("Country: " + targetCountry);
+// console.log("Top Level Domain: " + targetTLD(targetCountry));
 
 //A-sync
 jsonfilereader.readAndParse("./countries.json", function(importedJSON){
